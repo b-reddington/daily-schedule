8fabef5a5f7a254efefafe0f0e8e0ab74283f692
b-reddington <brandon.reddington98@gmail.com>
2023-03-20 21:45:48 -0700

    Added logic for detecting current hour

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,7 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// NOTE: This outside function stays. It readys your program when the document is fully loaded.
 $(function () {
   $("header").css("text-align", "center"); // Centers text in header
   // Extensions of DayJS
@@ -11,7 +10,7 @@ $(function () {
   dayjs.extend(window.dayjs_plugin_timezone); // This plugin adds support for time zones
 
   // Display Time in header
-  updateTime();
+  updateTime(); // Calls function to update time
   setInterval(updateTime, 1000); // Updates every 1 second
 
   function updateTime() {
@@ -19,4 +18,18 @@ $(function () {
     $("#currentDay").text(today.format("dddd, MMMM Do h:mm A"));
   }
 
+  //Code to update each time block style based on time of day
+$(".time-block").each(function () {
+  var hour = dayjs().tz("America/Los_Angeles").hour(); // Grabbed from dayJs documentation
+  // grabs value of the id, splits it into an array, and converts the string into an integer
+  var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+
+  if (timeBlock < hour) {
+    $(this).removeClass("present future").addClass("past"); // if current time is past time-block id, add class past
+  } else if (timeBlock === hour) {
+    $(this).removeClass("past future").addClass("present"); // if current time is equal to time block id, add class present
+  } else {
+    $(this).removeClass("past present").addClass("future"); // if time is not currently equal or past time-block id, add class future
+  }
+});
 });
