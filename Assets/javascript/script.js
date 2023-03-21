39de48964b90e46e2cf1676d86754ac78f9ce322
b-reddington <brandon.reddington98@gmail.com>
2023-03-20 21:08:44 -0700

    Added updated scripts for dayjs, added functional clock in header

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,27 +2,21 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+// NOTE: This outside function stays. It readys your program when the document is fully loaded.
 $(function () {
-  $("#hour-9").click(function() {
-    console.log("test")
-  });
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  $("header").css("text-align", "center"); // Centers text in header
+  // Extensions of DayJS
+  dayjs.extend(window.dayjs_plugin_advancedFormat);
+  dayjs.extend(window.dayjs_plugin_utc); // This plugin adds support for UTC mode
+  dayjs.extend(window.dayjs_plugin_timezone); // This plugin adds support for time zones
 
+  // Display Time in header
+  updateTime();
+  setInterval(updateTime, 1000); // Updates every 1 second
+
+  function updateTime() {
+    var today = dayjs().tz("America/Los_Angeles");
+    $("#currentDay").text(today.format("dddd, MMMM Do h:mm A"));
+  }
+
+});
